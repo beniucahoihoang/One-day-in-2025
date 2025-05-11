50bf33c0262881e68a4e8b923795c1387bcbf7aa
beniucahoihoang <khang.huynh.210218@student.fulbright.edu.vn>
2025-05-12 01:05:28 +0700

    Create style.css

---
+++ style.css
@@ -0,0 +1,16 @@
+body {
+  margin: 0;
+  font-family: sans-serif;
+  background: #f0f0f0;
+}
+
+main {
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  background: white;
+  padding: 10px 15px;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0,0,0,0.1);
+  z-index: 100;
+}
